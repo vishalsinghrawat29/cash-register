91dc2c30f1c698f37a920887c3f25ea3898da2c6
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-09-01 19:59:17 +0530

    update responsive

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const billAmount = document.querySelector("#bill-Amount");
 const cashGiven = document.querySelector("#cash-Given");
-const cashGivenLabel = document.querySelector("#cash-Given-label");
 const checkButton = document.querySelector("#check-button");
 const nextButton = document.querySelector("#next-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const cashGivenSection = document.querySelector('.section-cash-given');
+const outputSection = document.querySelector(".output-section");
 const availableNotes = [2000,500,100,20,10,5,1];
-hideCashGiven()
+hideCashGivenAndOutput()
 nextButton.addEventListener("click", function showCashGivenField(){
     showCashGiven();
 });
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
     hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value ){
+    if(Number(billAmount.value) > 0){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
             const amountToBeReturned  = cashGiven.value - billAmount.value;
+            outputSection.style.display = 'block'
             calculateChange(amountToBeReturned);
         }else{
             showMessage("The cash provided should at least be equal to the bill amount");
@@ -32,13 +34,20 @@ function showMessage(msg){
 function hideMessage(){
     message.style.display = "none";
 }
-function hideCashGiven(){
-    cashGiven.style.display = "none";
-    cashGivenLabel.style.display = "none";
+function hideCashGivenAndOutput(){
+    cashGivenSection.style.display = "none";
+    outputSection.style.display = "none";
 }
 function showCashGiven(){
-    cashGiven.style.display = "block";
-    cashGivenLabel.style.display = "block";
+    if (billAmount.value > 0){
+        cashGivenSection.style.display = 'block'
+        message.innerText = ""   
+    } else {
+        showMessage("Invalid value") // calling a showMessage function
+        cashGivenSection.style.display = 'none'   
+        outputSection.style.display = 'none'     
+      }
+
 }
 
 function calculateChange(amountToBeReturned){
